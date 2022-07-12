8876cce233c71c73133811559739641b405607b7
Henry <Henry_Koleoso@hotmail.co.uk>
2022-07-12 21:31:09 +0100

    adding a new bot instance

--- live-instances/main.tf
+++ live-instances/main.tf
@@ -8,3 +8,14 @@ module "vultr-instances" {
   hostname = "test-bot"
   api_key = var.api_key
 }
+
+module "vultr-instances-2" {
+  source = "../vultr-instance"
+  plan = "vc2-1c-1gb"
+  region = "lhr"
+  os_id = 477
+  label = "test-bot-2"
+  tags = ["testing-2"]
+  hostname = "test-bot-2"
+  api_key = var.api_key
+}
